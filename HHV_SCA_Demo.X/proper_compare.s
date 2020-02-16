d9158941bfe5cfb16ddb2c50e3c20df029e9bc54
Kris Bahnsen <Kris@KBEmbedded.com>
2020-02-16 00:19:07 -0800

    proper_compare: Convert to UNIX newlines
    It was bothering me

--- HHV_SCA_Demo.X/proper_compare.s
+++ HHV_SCA_Demo.X/proper_compare.s
@@ -7,19 +7,19 @@
  * linker bitching that they overlap even though they should be relocated.
  * So, just make them global in C, in common RAM, I guess.
   */
-    
+
 /* Returns 0 when strings match, greater than 0 when differing */
 global	_pin
 global	_pin_input
 global	_loop
 global	_cmp
-	    
+
 psect   comparefunc,class=CODE,delta=2
-	    
+
 /* Confession: It has been a while since I wrote assembly, so this
  * is not the most optimized. However, it is exactly the same amount
  * of cycles for each digit in the PIN, correct or incorrect.
- * 
+ *
  * The critical section marked below, branches to one of two paths
  * that meet back up later to the same place. The branch is
  * determined by a comparison of one byte from the input PIN and
@@ -29,11 +29,11 @@ psect   comparefunc,class=CODE,delta=2
  * take exactly the same amount of cycles. Doing so eliminates the
  * timing side channel attack (there may still be other avenues,
  * however).
- * 
+ *
  * Both the input PIN and the expected PIN, as well as the loop
  * counter are put in specific places of common RAM. This might not
  * actually be necessary, however it has little impact overall.
- * It prevents this assembly section and the compiler from having to worry 
+ * It prevents this assembly section and the compiler from having to worry
  * about bank switching to access these variables. This may not be a
  * concern because:
  * 1) RAM use is small enough for the whole project that all of the RAM fits
@@ -42,11 +42,11 @@ psect   comparefunc,class=CODE,delta=2
  *      touched inside of this block and put them all in the same bank to
  *      begin with.
  */
-	    
+
 global _proper_compare
 
 _proper_compare:
-       
+
     CLRF    _cmp
     MOVLW   3;
     MOVWF   _loop;
@@ -56,7 +56,7 @@ stage_3_loop:
     ADDWF   _loop, W
     MOVWF   FSR0L
     CLRF    FSR0H
-    
+
     /* Load the address of pin+3 in to FSR1 */
     MOVLW   low _pin
     ADDWF   _loop, W
@@ -77,7 +77,7 @@ check_loop:			//From BTFSS, both paths take 2 cycles to here.
     XORWF   _loop, W;
     BTFSC   STATUS, 2;		//2 cycles if true, 1 if false. Bit 2 is Z
     GOTO    end_loop;
-            
+
     DECF    _loop, F;
     GOTO    stage_3_loop;
 end_loop:
